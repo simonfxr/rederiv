0393cbc26e222bdd646fb84e8be73aeebdca84a0
Simon Reiser <me@sfxr.de>
2020-06-14 18:20:52 +0200

    Fix formatting

--- src/main/java/de/sfxr/rederiv/DFA.java
+++ src/main/java/de/sfxr/rederiv/DFA.java
@@ -30,19 +30,19 @@ public class DFA {
         return newState(q, builder.accepting.contains(q));
     }
 
-    private static final OrderedSemigroup<Integer> INTS =
-            new OrderedSemigroup<Integer>() {
-                @Override
-                public int compare(Integer x, Integer y) {
-                    return Integer.compare(x, y);
-                }
+    private static final OrderedSemigroup<Integer> INTS = new OrderedSemigroup<Integer>() {
+        @Override
+        public int compare(Integer x, Integer y) {
+            return Integer.compare(x, y);
+        }
 
-                @Override
-                public Integer apply(Integer x, Integer y) {
-                    if (x == y) return x;
-                    throw new IllegalStateException("IMPOSSIBLE");
-                }
-            };
+        @Override
+        public Integer apply(Integer x, Integer y) {
+            if (x == y)
+                return x;
+            throw new IllegalStateException("IMPOSSIBLE");
+        }
+    };
 
     private DFA(DFABuilder builder, Re re) {
         this.trans = new IntervalSet[builder.Q.size()];
@@ -54,37 +54,27 @@ public class DFA {
             if (d == null) {
                 trans[i] = IntervalSet.empty();
             } else {
-                trans[i] =
-                        d.entrySet().stream()
-                                .map(
-                                        e ->
-                                                e.getKey()
-                                                        .toIntervalSet()
-                                                        .map(
-                                                                ignored ->
-                                                                        mkDFAState(
-                                                                                builder,
-                                                                                e.getValue()),
-                                                                INTS))
-                                .collect(
-                                        Collectors.reducing(
-                                                IntervalSet.<Integer>empty(),
-                                                (x, y) -> x.union(y, INTS)));
+                trans[i] = d.entrySet().stream()
+                        .map(e -> e.getKey().toIntervalSet().map(ignored -> mkDFAState(builder, e.getValue()), INTS))
+                        .collect(Collectors.reducing(IntervalSet.<Integer>empty(), (x, y) -> x.union(y, INTS)));
             }
         }
     }
 
     public boolean matches(String s) {
-        if (s == null) throw new NullPointerException();
+        if (s == null)
+            throw new NullPointerException();
 
         var x = initial;
         int cp;
 
         for (int i = 0; !isAcceptingState(x); i += Character.charCount(cp)) {
-            if (i >= s.length()) return false;
+            if (i >= s.length())
+                return false;
             cp = s.codePointAt(i);
             var xx = trans[stateQ(x)].find(cp);
-            if (xx == null) return false;
+            if (xx == null)
+                return false;
             x = xx;
         }
 
